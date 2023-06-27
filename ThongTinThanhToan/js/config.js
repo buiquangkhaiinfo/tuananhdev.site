5b6a5cb6642297ed00dcd857c067c03989696a6d
buiquangkhaiinfo <122881045+buiquangkhaiinfo@users.noreply.github.com>
2023-06-27 19:49:31 +0700

    Add files via upload

---
+++ ThongTinThanhToan/js/config.js
@@ -0,0 +1,18 @@
+const USER = {
+    banks: [{
+            name: "MB BANK",
+            logo: "./images/mbbank.png",
+            number: "Không có ",
+            owner: "THAI DINH TUAN ANH"
+        },
+      
+        {
+            name: "MOMO",
+            logo: "./images/momo.jpeg",
+            number: "0786610049",
+            owner: "Thái Đinh Tuấn Anh",
+            fastTransfer: true
+        },
+       
+    ]
+};
